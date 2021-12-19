e480f60a8cfcc993a426ed9bee42b622362e2105
David Canales Páez <david.canalespaez@gmail.com>
2021-12-18 23:09:35 -0600

    fusion de ramas y aplicacion de tailwind a formulario, header y listadoPac

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -4,39 +4,39 @@ const Formulario = () => {
         <div class= 'md: w-1/2 lg:w-2/5'>
             <h2 className='font-black text-3xl text-center'>Seguimiento de Pacientes</h2>
             <p className='text-lg mt-5 text-center mb-10'>Añade pacientes y {' '}
-                <span class='text-indigo-600 font-bold'>adminístralos</span>
+                <span className='text-indigo-600 font-bold'>adminístralos</span>
             </p>
 
-            <form class='bg-white shadow-md rounded-lg py-10 px-5'>
-                <div class='mb-5'>
-                    <label htmlFor="mascota" class='block text-gray-700 uppercase font-bold'>Nombre Mascota</label>
+            <form className='bNameg-white shadow-md rounded-lg py-10 px-5'>
+                <div className='mb-5'>
+                    <label htmlFor="mascota" className='block text-gray-700 uppercase font-bold'>Nombre Mascota</label>
                     <input 
                         type='text'
                         placeholder="Nombre de la mascota"
-                        class='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                         id= 'mascota'
                     />
                 </div>
-                <div class='mb-5'>
+                <div className='mb-5'>
                     <label htmlFor="mascota" class='block text-gray-700 uppercase font-bold'>Propietario</label>
                     <input 
                         type='text'
                         placeholder="Nombre del propietario"
-                        class='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                         id= 'propietario'
                     />
                 </div>
-                <div class='mb-5'>
-                    <label htmlFor="e-mail" class='block text-gray-700 uppercase font-bold'>Correo electrónico</label>
+                <div className='mb-5'>
+                    <label htmlFor="e-mail" className='block text-gray-700 uppercase font-bold'>Correo electrónico</label>
                     <input 
                         type='text'
                         placeholder="e-mail de contacto"
-                        class='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                         id= 'e-mail'
                     />
                 </div>
-                <div class='mb-5'>
-                    <label htmlFor="Alta" class='block text-gray-700 uppercase font-bold'>Fecha Ingreso del paciente</label>
+                <div className='mb-5'>
+                    <label htmlFor="Alta" className='block text-gray-700 uppercase font-bold'>Fecha Ingreso del paciente</label>
                     <input
                         type='date'
                         id= 'Alta'
@@ -44,11 +44,11 @@ const Formulario = () => {
                 </div>
 
                 <div class='mb-5'>
-                    <label htmlFor="Síntomas" class='block text-gray-700 uppercase font-bold'>Síntomas</label>
+                    <label htmlFor="Síntomas" className='block text-gray-700 uppercase font-bold'>Síntomas</label>
                     <textarea
                         id= 'Alta'
                         placeholder="Describe los sítomas"
-                        class='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                     />
                 </div>
 
--- src/ListadoPacientes.jsx
+++ src/ListadoPacientes.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 const ListadoPacientes = () => {
     return (
         <div class='w-1/2 lg:w-3/5 h-screen overflow-y-scroll'>
-            <h2 class='font-black text-3xl text-center'>Listado de Pacientes</h2>
-            <p class='text-xl mt-5 mb-10 text-center'>Administra tus {' '}
-            <span class='text-indigo-600 font-bold'>pacientes y citas</span></p>
-            <div class='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
-                <p class='font-normal mb-3 text-gray-700 uppercase'>Nombre:{' '}
-                    <span class='font-bold normal-case'>Hook</span>
+            <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
+            <p className='text-xl mt-5 mb-10 text-center'>Administra tus {' '}
+            <span className='text-indigo-600 font-bold'>pacientes y citas</span></p>
+            <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
+                <p className='font-normal mb-3 text-gray-700 uppercase'>Nombre:{' '}
+                    <span className='font-bold normal-case'>Hook</span>
                 </p>
 
-                <p class='font-normal mb-3 text-gray-700 uppercase'>Dueño:{' '}
-                    <span class='font-bold normal-case'>Juan Camaney</span>
+                <p className='font-normal mb-3 text-gray-700 uppercase'>Dueño:{' '}
+                    <span className='font-bold normal-case'>Juan Camaney</span>
                 </p>
 
-                <p class='font-normal mb-3 text-gray-700 uppercase'>e-mail:{' '}
+                <p className='font-normal mb-3 text-gray-700 uppercase'>e-mail:{' '}
                     <span class='font-bold normal-case'>juan@correo.com</span>
                 </p>
 
-                <p class='font-normal mb-3 text-gray-700 uppercase'>Ingreso:{' '}
-                    <span class='font-bold normal-case'>12/12/2021</span>
+                <p className='font-normal mb-3 text-gray-700 uppercase'>Ingreso:{' '}
+                    <span className='font-bold normal-case'>12/12/2021</span>
                 </p>
 
-                <p class='font-normal mb-3 text-gray-700 uppercase'>Síntomas:{' '}
-                    <span class='font-bold normal-case'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis obcaecati facere eveniet voluptatem, culpa voluptate quia, ipsa at ad accusamus numquam! Commodi nobis inventore enim ex a repellat, laborum, cum dolorum amet dolore adipisci voluptates. Et aperiam odio dolorem inventore, voluptatem eius accusantium alias neque nam reiciendis ad, quae qui.</span>
+                <p clasNames='font-normal mb-3 text-gray-700 uppercase'>Síntomas:{' '}
+                    <span className='font-bold normal-case'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis obcaecati facere eveniet voluptatem, culpa voluptate quia, ipsa at ad accusamus numquam! Commodi nobis inventore enim ex a repellat, laborum, cum dolorum amet dolore adipisci voluptates. Et aperiam odio dolorem inventore, voluptatem eius accusantium alias neque nam reiciendis ad, quae qui.</span>
                 </p>
             </div>
         </div>
