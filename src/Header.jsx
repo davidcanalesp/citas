8798f4b0ca4ae21263018e2c4f98aaf25ed3f503
David Canales Páez <david.canalespaez@gmail.com>
2021-12-18 19:12:05 -0600

    agregué estilos tailwind al encabezado

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
     return (
         <div >
-            <h1 class='font-black text-5xl text-center'>Seguimiento Pacientes 
-                <span>
+            <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimiento Pacientes {' '}
+                <span className='text-indigo-600'>
                     Veterinaria
                 </span> 
             </h1>
