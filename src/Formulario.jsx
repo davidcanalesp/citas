9bfdeea4fd5282fc12d092c3c06e085b7bd72bf4
David Canales Páez <david.canalespaez@gmail.com>
2021-12-22 12:57:26 -0600

    guardé en los estados lo que se ingresa en el formulario

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 const Formulario = () => {
+    const [nombre, setNombre]= useState('');
+    const [propietario, setPropietario]= useState('');
+    const [email, setEmail]= useState('');
+    const [fecha, setFecha]= useState('');
+    const [sintoma, setSintoma]= useState('');
+
+    const handleSubmit= (e) => {
+        //validación del formulario
+        e.preventDefault();
+    }
 
     return ( 
         <div class= 'md: w-1/2 lg:w-2/5'>
             <h2 className='font-black text-3xl text-center'>Seguimiento de Pacientes</h2>
             <p className='text-lg mt-5 text-center mb-10'>Añade pacientes y {' '}
-                <span className='text-indigo-600 font-bold'>adminístralos</span>
+                <span className='text-indigo-600 font-bold'>adminístralos {nombre}</span>
             </p>
 
-            <form className='bNameg-white shadow-md rounded-lg py-10 px-5'>
+            <form className='bNameg-white shadow-md rounded-lg py-10 px-5'
+                  onSubmit={handleSubmit}
+            
+            >
                 <div className='mb-5'>
                     <label htmlFor="mascota" className='block text-gray-700 uppercase font-bold'>Nombre Mascota</label>
                     <input 
@@ -15,6 +29,8 @@ const Formulario = () => {
                         placeholder="Nombre de la mascota"
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                         id= 'mascota'
+                        value={nombre}
+                        onChange={(e)=>setNombre(e.target.value)}
                     />
                 </div>
                 <div className='mb-5'>
@@ -24,15 +40,19 @@ const Formulario = () => {
                         placeholder="Nombre del propietario"
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                         id= 'propietario'
+                        value={propietario}
+                        onChange={(e)=>setPropietario(e.target.value)}
                     />
                 </div>
                 <div className='mb-5'>
                     <label htmlFor="e-mail" className='block text-gray-700 uppercase font-bold'>Correo electrónico</label>
                     <input 
-                        type='text'
+                        type='email'
                         placeholder="e-mail de contacto"
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
                         id= 'e-mail'
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}
                     />
                 </div>
                 <div className='mb-5'>
@@ -40,15 +60,19 @@ const Formulario = () => {
                     <input
                         type='date'
                         id= 'Alta'
+                        value={fecha}
+                        onChange={(e)=>setFecha(e.target.value)}
                     />
                 </div>
 
                 <div class='mb-5'>
-                    <label htmlFor="Síntomas" className='block text-gray-700 uppercase font-bold'>Síntomas</label>
+                    <label htmlFor="Sintomas" className='block text-gray-700 uppercase font-bold'>Síntomas</label>
                     <textarea
-                        id= 'Alta'
+                        id= 'Sintoma'
                         placeholder="Describe los síntomas"
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                        value={sintoma}
+                        onChange={(e)=>setSintoma(e.target.value)}
                     />
                 </div>
 
---
+++ src/Paciente.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Paciente = () => {
+    return (
+        <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
+                <p className='font-normal mb-3 text-gray-700 uppercase'>Nombre:{' '}
+                    <span className='font-bold normal-case'>Hook</span>
+                </p>
+
+                <p className='font-normal mb-3 text-gray-700 uppercase'>Dueño:{' '}
+                    <span className='font-bold normal-case'>Juan Camaney</span>
+                </p>
+
+                <p className='font-normal mb-3 text-gray-700 uppercase'>e-mail:{' '}
+                    <span class='font-bold normal-case'>juan@correo.com</span>
+                </p>
+
+                <p className='font-normal mb-3 text-gray-700 uppercase'>Ingreso:{' '}
+                    <span className='font-bold normal-case'>12/12/2021</span>
+                </p>
+
+                <p clasNames='font-normal mb-3 text-gray-700 uppercase'>Síntomas:{' '}
+                    <span className='font-bold normal-case'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis obcaecati facere eveniet voluptatem, culpa voluptate quia, ipsa at ad accusamus numquam! Commodi nobis inventore enim ex a repellat, laborum, cum dolorum amet dolore adipisci voluptates. Et aperiam odio dolorem inventore, voluptatem eius accusantium alias neque nam reiciendis ad, quae qui.</span>
+                </p>
+            </div>
+            
+        
+    )
+}
+
+export default Paciente
